b2f3c8f8db2961a662ae219f6ba5a67ec54eae04
fatawaimamalmuftin <fatawaimamalmuftin17@gmail.com>
2026-09-17 22:04:10 +0700

    doc: day 3 db

--- query3.sql
+++ query3.sql
@@ -53,3 +53,74 @@ JOIN actors a
 ON ma.actor_id = a.id_actor
 GROUP BY
 m.id_movie, m.title;
+
+-- Exercise
+SELECT 
+p1.product_name AS Product,
+p1.price AS Price
+FROM products_pagi p1
+WHERE p1.price > (
+    SELECT AVG(p2.price)
+    FROM products_pagi p2
+);
+
+SELECT
+ts.customer_id AS ID_CUS,
+ts.amount AS Jumlah
+FROM transactions ts
+WHERE ts.amount > (
+    SELECT AVG(ts2.amount)
+    FROM transactions ts2
+    WHERE ts.customer_id = ts2.customer_id
+);
+
+-- Exercise 1
+WITH total_pembelian AS(
+    SELECT 
+    customer_id,
+    product,
+    SUM(quantity * price_per_unit) AS Total
+    FROM sales_pagi 
+    WHERE product = 'Keyboard'
+    GROUP BY customer_id, product
+)
+SELECT
+customer_id AS ID_CUS,
+product AS Product,
+Total AS Total
+FROM total_pembelian
+WHERE Total > 30;
+
+-- Exercise 2
+WITH ter_jual_1kali AS(
+    SELECT
+    product
+    FROM sales_pagi
+    GROUP BY product
+    HAVING COUNT(*) = 1
+)
+SELECT
+s.customer_id AS ID_CUS,
+s.product AS Product,
+s.quantity AS Qty
+FROM sales_pagi s
+JOIN ter_jual_1kali t
+ON s.product = t.product;
+
+-- Test
+WITH tblTTL AS(
+    SELECT
+    p.name AS Name,
+    p.price AS Price,
+    SUM(s.quantity) AS Qty,
+    SUM(p.price * s.quantity) AS Total
+    FROM products p
+    JOIN sales s
+    ON p.id = s.product_id
+    GROUP BY
+    p.name, p.price
+)
+SELECT
+name, price, total
+FROM tblTTL
+WHERE qty >= 7;
